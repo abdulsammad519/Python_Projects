1578faf617343aa6c716e80e185518c1dd0bfff4
Abdul Sammad <aabdulsammad57@gmail.com>
2026-01-10 21:15:21 +0500

    Uncomment movie ticket price calculation code

--- 02_Conditionals_Practice.py
+++ 02_Conditionals_Practice.py
@@ -1,11 +1,12 @@
 # Problem#02: Movie Ticket Price
 # Solution:
-# age=15
-# day="wednesday"
-# if age>=18:
-#    price=12
-# else:
-#    price=8
-# if day=="wednesday":
-#    price=price-2
-#    print(price)
+ age=15
+ day="wednesday"
+ if age>=18:
+    price=12
+ else:
+    price=8
+ if day=="wednesday":
+    price=price-2
+    print(price)
+
