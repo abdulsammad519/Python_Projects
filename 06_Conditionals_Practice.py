62b22363a0557090735664aa0d00cc07c65ba210
Abdul Sammad <aabdulsammad57@gmail.com>
2026-01-10 21:18:42 +0500

    Uncomment transportation mode logic

--- 06_Conditionals_Practice.py
+++ 06_Conditionals_Practice.py
@@ -1,13 +1,12 @@
 # Problem 06: Mode_of_Transportation according to Distance
 # Solution:
   
-# Distance=43
-# if Distance<3:
-#    Mode_of_Transportation= "walk"
-# elif Distance<=15:
-#    Mode_of_Transportation= "bike"
-# else:
-#    Mode_of_Transportation= "car"
+ Distance=43
+ if Distance<3:
+    Mode_of_Transportation= "walk"
+ elif Distance<=15:
+    Mode_of_Transportation= "bike"
+ else:
+    Mode_of_Transportation= "car"
+ print(Mode_of_Transportation)
 
-
-# print(Mode_of_Transportation)
