e451cdd2c1379df498e4c156bbcab777a3e35897
Abdul Sammad <aabdulsammad57@gmail.com>
2026-01-10 21:17:11 +0500

    Uncomment fruit ripeness checking code

--- 04_Conditionals_Practice.py
+++ 04_Conditionals_Practice.py
@@ -1,15 +1,14 @@
 # Problem 04:Checking of fruit ripeness
 # Solution:
 
-# color="Yellow"
-# Fruit="Bananna"
-# if color=="Green":
-#    Taste="unripe"
-# elif color=="Yellow":
-#    Taste="ripe"
-# elif color=="Brown":
-#    Taste="overripe"
-# else:
-#    print("on the base of your fruit and with given color, taste is not in our knowledge")
-
-# print(Taste)
+ color="Yellow"
+ Fruit="Bananna"
+ if color=="Green":
+    Taste="unripe"
+ elif color=="Yellow":
+    Taste="ripe"
+ elif color=="Brown":
+    Taste="overripe"
+ else:
+    print("on the base of your fruit and with given color, taste is not in our knowledge")
+ print(Taste)
