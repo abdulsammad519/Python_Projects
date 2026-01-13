383ec710b3d13ef02e20dd6dc41bbf2914d9d616
Abdul Sammad <aabdulsammad57@gmail.com>
2026-01-13 21:34:54 +0500

    multiplication of numbers with the concept of polymorphism

---
+++ 03_Functions_Practice.py
@@ -0,0 +1,6 @@
+#multiplication of numbers with the concept of polymorphism
+#solution:
+def multiply(a,b):
+  return a*b
+result=multiply(5,6)      #Polymorphism says: multiply(5,6)==multiply(6,5)
+print(result)
