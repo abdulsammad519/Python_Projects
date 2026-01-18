c40c1e9bf79cdaa00d79acedb5ace98fe66fe7c3
Abdul Sammad <aabdulsammad57@gmail.com>
2026-01-18 16:36:50 +0500

    Cache Returns Values

---
+++ 03_Decorators_Practice .py
@@ -0,0 +1,23 @@
+#Cache Returns Values:
+#(Implement a decorator that caches the return values of a function, so that when it is called with the same arguments, 
+# the cached value is returned instead of re-executing the function.)
+
+import time
+def cache(func):
+  cache_value={} #dictionary can be accessed easily instead of lists 
+  print(cache_value)
+  def wrapper(*args):
+    if args in cache_value:
+       return cache_value[args]
+    result=func(*args)
+    cache_value[args]=result
+    return result
+  return wrapper
+
+@cache
+def long_running_function(a,b):
+    time.sleep(4)
+    return a+b
+print(long_running_function(2,3))
+print(long_running_function(2,3))
+print(long_running_function(4,2))
